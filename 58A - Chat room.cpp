93041eb596fd21e44f99eacee3d5a10345e5deda
sarangbishal <sarangbishal@gmail.com>
2017-12-20 12:03:15 +0545

    Update 58A - Chat room.cpp

--- 58A - Chat room.cpp
+++ 58A - Chat room.cpp
@@ -37,42 +37,4 @@ if(count == 5)
 else
 	cout << "NO";
 	return 0;
-}#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	// your code goes here
-	string s;
-	cin >> s;
-	string t = "hello";
-	int n = s.length();
-	int count = 0;
-	if(n < t.length())
-		{
-			cout << "NO";
-			return 0;
-		}
-	else
-	{
-		int i = 0;
-			for(int j = 0 ; j < 5; j++)
-		{
-				for(; i < n; i++)
-	{
-		if(t[j] == s[i])
-			{
-			count++;
-			i++;
-			break;
-			}
-	
-	}
-	}	
-	}
-		
-if(count == 5)
-	cout << "YES";
-else
-	cout << "NO";
-	return 0;
 }
