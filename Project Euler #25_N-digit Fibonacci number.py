16153eeb4c31b8ff66edc1eeda04099507d00d2d
sarangbishal <sarangbishal@gmail.com>
2018-01-02 18:12:27 +0545

    Add files via upload

--- Project Euler #25_N-digit Fibonacci number.py
+++ Project Euler #25_N-digit Fibonacci number.py
@@ -1,15 +1,18 @@
-a, b, length = 0, 1, 1
-store = { } #dictionry that store key(num_of digits) and value(smallest possible numbers)
+a, b, length, i = 0, 1, 1, 0
+store = { }
 store[length] = a
-fib = { } #dictionry that store key(N) and corresponding fibonacci number
-for i in range(35300): #35300 is choosen from hit and trial
+fib = { }
+
+while len(str(a)) < 5000:
     fib[i] = a
     if(len(str(fib[i])) > length):
         length = length + 1
+        #print length
         store[length] = i
+    i = i + 1
     a,b = b, a + b;
-for i in range(int(raw_input().strip())): 
-               print store[(int(raw_input().strip()))] #Print value stored at particular key
+for i in range(int(raw_input().strip())):
+               print store[(int(raw_input().strip()))]
     
     
     
