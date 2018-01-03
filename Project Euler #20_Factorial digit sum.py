efb4237e9830cd06c8fbbe1f6eb34a976cb79915
sarangbishal <sarangbishal@gmail.com>
2018-01-03 20:13:53 +0545

    Delete Project Euler #20_Factorial digit sum.py

--- Project Euler #20_Factorial digit sum.py
+++
@@ -1,8 +0,0 @@
-# Author: Bishal Sarang
-for i in range(int(raw_input().strip())): #Testcases
-    fact , sum = 1, 0
-    for j in range(1,int(raw_input().strip()) + 1): #factorial of nth number
-        fact = fact * j
-    for char in str(fact): #Extract each characters from results and typecast to integer and calculates sum
-        sum += int(char)
-    print sum
