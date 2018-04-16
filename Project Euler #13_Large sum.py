b192c2b6b3eb1efd7f34f7a00bd67b4408721667
Bishal Sarangkoti <sarangbishal@gmail.com>
2018-04-16 23:11:36 +0545

    Delete Project Euler #13_Large sum.py

--- Project Euler #13_Large sum.py
+++
@@ -1,6 +0,0 @@
-# Author : Bishal Sarang
-sum = 0;
-for i in range(int(raw_input().strip())): #Value of N
-     sum += int(raw_input().strip()); #sum of numbers
-sum = str(sum) #typecast to string
-print sum[0: 10] #print first 10 digits
